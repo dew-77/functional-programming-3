274f0150a2cee2a7defb6bb70a99d4f591177eb9
Artem <yurkov.artyom21@gmail.com>
2025-03-19 22:55:32 +0300

    Done lab 3

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+// Функция, которая принимает массив чисел и возвращает новый массив, содержащий только числа, кратные заданному числу.
 function getMultiplesBy(array, by) {
     return array.filter(function (item) { return item % by === 0; });
 }
@@ -5,3 +15,48 @@ var nums = [1, 2, 3, 4, 5];
 var divisible_by = 2;
 var answer = getMultiplesBy(nums, divisible_by);
 console.log("\u0427\u0438\u0441\u043B\u0430 \u043C\u0430\u0441\u0441\u0438\u0432\u0430 ".concat(nums, ", \u0434\u0435\u043B\u044F\u0449\u0438\u0435\u0441\u044F \u043D\u0430 ").concat(divisible_by, ": ").concat(answer));
+// Функция, которая принимает массив строк и возвращает новую строку, содержащую все строки, объединенные заданным разделителем.
+function customJoin(strings, separator) {
+    return strings.join(separator);
+}
+var words = ["searching", "for", "ideas", "to", "write", "text"];
+var result = customJoin(words, " ");
+console.log(result);
+// Функция, которая принимает массив объектов и возвращает новый массив, отсортированный по значению определенного свойства.
+function sortByProperty(items, property) {
+    return __spreadArray([], items, true).sort(function (a, b) {
+        if (a[property] < b[property]) {
+            return -1;
+        }
+        if (a[property] > b[property]) {
+            return 1;
+        }
+        return 0;
+    });
+}
+var phones = [
+    { brand: "Apple", model: "iPhone 15", price: 699 },
+    { brand: "Samsung", model: "Galaxy S22", price: 799 },
+    { brand: "Google", model: "Pixel 9", price: 899 }
+];
+var sortedByPrice = sortByProperty(phones, "price");
+console.log(sortedByPrice);
+// Функция, которая принимает другую функцию в качестве аргумента и возвращает новую функцию, которая выполняет логирование перед вызовом исходной функции.
+function addLogging(fn) {
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        var functionName = fn.name || 'anonymous function';
+        console.log("\u0412\u044B\u0437\u043E\u0432 \u0444\u0443\u043D\u043A\u0446\u0438\u0438 ".concat(functionName, " \u0441 \u0430\u0440\u0433\u0443\u043C\u0435\u043D\u0442\u0430\u043C\u0438:"), args);
+        var result = fn.apply(void 0, args);
+        console.log("\u0424\u0443\u043D\u0446\u043A\u0438\u044F ".concat(functionName, " \u0432\u0435\u0440\u043D\u0443\u043B\u0430:"), result);
+        return result;
+    };
+}
+function add(a, b) {
+    return a + b;
+}
+var funcWithLogging = addLogging(add);
+funcWithLogging(3, 5);
--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 // Функция, которая принимает массив чисел и возвращает новый массив, содержащий только числа, кратные заданному числу.
-
 function getMultiplesBy<NumbersType extends number>(array: NumbersType[], by: NumbersType): NumbersType[] {
     return array.filter((item) => item % by === 0);
 }
@@ -9,6 +8,61 @@ const divisible_by: number = 2;
 const answer: number[] = getMultiplesBy(nums, divisible_by)
 console.log(`Числа массива ${nums}, делящиеся на ${divisible_by}: ${answer}`)
 
-// Функция, которая принимает массив строк и возвращает новую строку, содержащую все строки, объединенные заданным разделителем.
 
-function customJoin()
+// Функция, которая принимает массив строк и возвращает новую строку, содержащую все строки, объединенные заданным разделителем.
+function customJoin(strings: string[], separator: string): string {
+    return strings.join(separator);
+  }
+
+const words = ["searching", "for", "ideas", "to", "write", "text"];
+const result = customJoin(words, " ");
+console.log(result);
+
+
+// Функция, которая принимает массив объектов и возвращает новый массив, отсортированный по значению определенного свойства.
+function sortByProperty<T>(items: T[], property: keyof T): T[] {
+    return [...items].sort((a, b) => {
+      if (a[property] < b[property]) {
+        return -1;
+      }
+      if (a[property] > b[property]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+interface Phone {
+    brand: string;
+    model: string;
+    price: number;
+  }
+
+const phones: Phone[] = [
+{ brand: "Apple", model: "iPhone 15", price: 699 },
+{ brand: "Samsung", model: "Galaxy S22", price: 799 },
+{ brand: "Google", model: "Pixel 9", price: 899 }
+];
+
+const sortedByPrice = sortByProperty(phones, "price");
+console.log(sortedByPrice);
+
+
+// Функция, которая принимает другую функцию в качестве аргумента и возвращает новую функцию, которая выполняет логирование перед вызовом исходной функции.
+function addLogging<T extends (...args: any[]) => any>(fn: T): T {
+  return function (...args: Parameters<T>): ReturnType<T> {
+    const functionName = (fn as unknown as { name: string }).name || 'anonymous function';
+    console.log(`Вызов функции ${functionName} с аргументами:`, args);
+    const result = fn(...args);
+    console.log(`Фунцкия ${functionName} вернула:`, result);
+    return result;
+  } as T;
+}
+
+function add(a: number, b: number): number {
+return a + b;
+}
+
+const funcWithLogging = addLogging(add);
+
+funcWithLogging(3, 5);
